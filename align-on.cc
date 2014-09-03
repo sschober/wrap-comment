8fddc5627cf1ba9cbed1d181ba3f6fdd1efe4c3e
Sven Schober <sven.schober@uni-ulm.de>
2014-09-03 11:11:20 +0200

    String-Utils einsetzen

--- align-on.cc
+++ align-on.cc
@@ -1,41 +1,11 @@
 #include <unistd.h>
 #include <iostream>
-#include <string>
-#include <list>
+#include "string-utils.h"
 #include <tuple>
 using namespace std;
 
 #define   FILL_CHAR      ' '
 #define   PADDING       1
-
-string rtrim( string str ){                   // Wir initialisieren eine Laufvariable
-  size_t i = str.size();                      // mit dem Ende des Strings und zählen
-  while( i > 0 && isspace(str[i-1])) { --i; } // bis zum ersten Nichtleerzeichen
-  return str.erase(i,string::npos);           // zurück. Dann löschen wir alles
-}                                             // ab diesem Index.
-
-bool isWhiteSpace( string str ){ // Wir löschen alle Leerzeichen im String. Wenn
-  return rtrim(str).empty();     // er dann leer ist, bestand er nur aus Leerzeichen.
-}
-
-/**
- * Splitet `str` an `ws` auf und gibt Partikel/Wörter als Liste zurück
- */
-list<string> split(  string str, const char *ws = " " ) { // Konzeptionell iterieren
-  list<string> result;                                    // wir hier über `str`,
-  size_t idx = 0;                                         // in dem wir immer wieder
-  while( string::npos != ( idx = str.find(ws))){          // das nächste Leerzeichen
-    string word = str.substr(0,idx);                      // (`ws`) suchen und von
-    if( ! word.empty() ){                                 // 0 bis dort als neues
-      result.push_back( word );                           // Wort zur Ergebnisliste
-    }                                                     // hinzufügen.
-    str = str.substr(idx+1, string::npos);
-  }
-  if( ! str.empty() ){
-    result.push_back(str);
-  }
-  return result;
-}
 
 int main(int argc, char *argv[]){
   string delimString = "=";
@@ -68,10 +38,10 @@ int main(int argc, char *argv[]){
       continue;
     }
     string left = rtrim(line.substr(0, commentCharIdx )); // jede in einen Code-Teil
-    string right = line.substr(
+    string right = ltrim(line.substr(
           commentCharIdx+delimString.size(),
           string::npos
-          );
+          ));
     if(left.size() > maxCodeLine){                        // und einen Kommentarteil
       maxCodeLine = left.size();                          // auf. Die Code-Zeilen
     }                                                     // fügen wir zu _einer_
@@ -88,8 +58,9 @@ int main(int argc, char *argv[]){
       size_t padding = maxCodeLine - left.size();
       if( 0 < padding ){
         left.append( padding, ' ');
-        left.append( 1, '=');
-        left.append( PADDING, ' ');
+        left.append( delimString );
+        if( ! right.empty() && ! isWhiteSpace( right ))
+          left.append( PADDING, ' ');
       }
     }
     left.append( right );
--- wrap-comment.cc
+++ wrap-comment.cc
@@ -1,40 +1,10 @@
 #include <unistd.h>
 #include <iostream>
-#include <string>
-#include <list>
+#include "string-utils.h"
 using namespace std;
 
 #define   FILL_CHAR      ' '
 #define   PADDING       1
-
-string rtrim( string str ){                   // Wir initialisieren eine Laufvariable
-  size_t i = str.size();                      // mit dem Ende des Strings und zählen
-  while( i > 0 && isspace(str[i-1])) { --i; } // bis zum ersten Nichtleerzeichen
-  return str.erase(i,string::npos);           // zurück. Dann löschen wir alles
-}                                             // ab diesem Index.
-
-bool isWhiteSpace( string str ){ // Wir löschen alle Leerzeichen im String. Wenn
-  return rtrim(str).empty();     // er dann leer ist, bestand er nur aus Leerzeichen.
-}
-
-/**
- * Splitet `str` an `ws` auf und gibt Partikel/Wörter als Liste zurück
- */
-list<string> split(  string str, const char *ws = " " ) { // Konzeptionell iterieren
-  list<string> result;                                    // wir hier über `str`,
-  size_t idx = 0;                                         // in dem wir immer wieder
-  while( string::npos != ( idx = str.find(ws))){          // das nächste Leerzeichen
-    string word = str.substr(0,idx);                      // (`ws`) suchen und von
-    if( ! word.empty() ){                                 // 0 bis dort als neues
-      result.push_back( word );                           // Wort zur Ergebnisliste
-    }                                                     // hinzufügen.
-    str = str.substr(idx+1, string::npos);
-  }
-  if( ! str.empty() ){
-    result.push_back(str);
-  }
-  return result;
-}
 
 int main(int argc, char *argv[]){
   string commentString = "#";
